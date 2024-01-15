884f1d4613e3819c75f9284aab416bdb65614e83
Rahmi Aulia <115514227+RahmiAulia@users.noreply.github.com>
2024-01-15 20:28:47 +0700

    Add files via upload

---
+++ fluttergui/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\k\\Semester 5\\Sistem Terdistribusi\\fluttersisterrahmi" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=D:\\k\\Semester 5\\Sistem Terdistribusi\\fluttersisterrahmi"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\k\\Semester 5\\Sistem Terdistribusi\\fluttersisterrahmi\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\k\\Semester 5\\Sistem Terdistribusi\\fluttersisterrahmi"
+  "FLUTTER_TARGET=D:\\k\\Semester 5\\Sistem Terdistribusi\\fluttersisterrahmi\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\k\\Semester 5\\Sistem Terdistribusi\\fluttersisterrahmi\\.dart_tool\\package_config.json"
+)
